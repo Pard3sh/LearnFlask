fc83476b33b3d1475768b61c6ea18839c00b7232
Pard3sh7 <P3sh22@gmail.com>
2022-11-23 02:37:15 -0500

    changed up the formatting of the basic home page

--- tutorial1.py
+++ tutorial1.py
@@ -3,8 +3,9 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
+@app.route("/<name>")
+def home(name):
+    # we can have it send name as a parameter to the index.html file
     return render_template("index.html")
     # now instead of inline HTML we are rendering an HTML file
 
